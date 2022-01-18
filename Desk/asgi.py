559f6eae113c6ea10a8b31e2f50a96b3e0560795
jun048098 <96534680+jun048098@users.noreply.github.com>
2022-01-18 22:34:44 +0900

    project name change

--- Desk/asgi.py
+++ Desk/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Desk project.
+ASGI config for Desksite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Desk.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Desksite.settings')
 
 application = get_asgi_application()
--- Desk/wsgi.py
+++ Desk/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Desk project.
+WSGI config for Desksite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Desk.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Desksite.settings')
 
 application = get_wsgi_application()
